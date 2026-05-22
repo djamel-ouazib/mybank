efcec51b58024bbbbef85e6ebf38c1b28353e2b7
djamel-ouazib <djamel.ouazibb@gmail.com>
2026-05-22 16:20:37 +0200

    fix: remove unused imports in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
             <nav className="flex w-6xl py-4 items-center m-auto justify-between">
